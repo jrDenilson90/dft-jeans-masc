dc97d51a40157e75e973852f5bcbdd45d8e63051
Denilson Jose da Silva Junior <denilson.junior@029968-SP-MB.local>
2025-04-28 08:49:24 -0300

    [initial]

---
+++ src/script/imports.js
@@ -0,0 +1,16 @@
+// imports.js
+
+// Import obrigatório
+module.exports.main = [
+    'src/script/main.js', // Inclua o arquivo principal
+];
+
+// Inclua todos os scripts de templates
+module.exports.templates = [
+    'src/views/templates/teste/script.js',
+];
+
+// Inclua todos os scripts de componentes
+module.exports.components = [
+    'src/views/components/welcome/script.js',
+];
